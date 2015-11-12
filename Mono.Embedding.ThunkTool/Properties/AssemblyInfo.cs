9638c28e65704c25a709a00844a1ef86b437d69d
Robert Jordan <robertj@gmx.net>
2015-11-12 14:35:43 +0100

    initial import

---
+++ Mono.Embedding.ThunkTool/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mono.Embedding.ThunkTool")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mono.Embedding.ThunkTool")]
+[assembly: AssemblyCopyright("Copyright © AMMMa AG 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e2d4341d-13cb-4155-bd53-09b6d539a8ec")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
